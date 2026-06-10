f07a02238f17dc51b64b76be80e3c14564938a31
Jose Flavio Xavier <jflavioxavier@gmail.com>
2026-06-10 20:57:02 -0300

    cria o incio do consumo da api

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 // Arquivo para código javascript
+
+//lista vazia para receber os materias
+let listaDemateriais =[];
+
+
+// criando o botão adicionar
+const btnCadastrar = document.getElementById("btn-cadastrar");
+const tabela = document.getElementById("corpo-tabela");
+
+
+btnCadastrar.addEventListener(onclick, () =>{
+    const inputMaterial = document.getElementById("input-nome");
+    const inputQtd = document.getElementById("input-quantidade");
+
+    const material = inputMaterial.value;
+    const quantidade = inputQtd.value;
+
+    if(material === "" || quantidade ===""){
+        alert("Favor informar o material e a sua quantidade")
+        return;
+    }
+
+    addListaDeMaterias(material, quantidade);
+    inputMaterial.value = "";
+    inputQtd.value = "";
+})
+
