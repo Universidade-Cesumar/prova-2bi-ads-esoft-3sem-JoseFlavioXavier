ae70abf77ad5484df8105504c423989e1ffcd493
Jose Flavio Xavier <jflavioxavier@gmail.com>
2026-06-10 21:30:42 -0300

    funcao que envia para o banco e funcao que limpa apos add para o banco

--- main.js
+++ main.js
@@ -1,28 +1,58 @@
-// Arquivo para código javascript
+const url = "https://6a29f8bcf59cb8f65f1de3eb.mockapi.io/api/v1/Materiai";
 
-//lista vazia para receber os materias
-let listaDemateriais =[];
-
-
-// criando o botão adicionar
-const btnCadastrar = document.getElementById("btn-cadastrar");
+const btnCadastra = document.getElementById("btn-cadastrar");
 const tabela = document.getElementById("corpo-tabela");
 
+// Função para buscar e exibir os dados
+async function carregarTabela() {
+    try {
+        const respostaApi = await fetch(url);
+        
+        if (!respostaApi.ok) throw new Error("Erro ao buscar dados");
+        
+        const dados = await respostaApi.json();
 
-btnCadastrar.addEventListener(onclick, () =>{
+        tabela.innerHTML = "";
+
+        dados.forEach(item => {
+            const novaLinha = document.createElement("tr");
+            novaLinha.innerHTML = `
+                <td>${item.material}</td>
+                <td>${item.quantidade}</td>
+            `;
+            tabela.appendChild(novaLinha);
+        });
+    } catch (erro) {
+        console.error("Erro na operação:", erro);
+    }
+}
+
+// Função para enviar os dados
+btnCadastra.addEventListener('click', async () => {
     const inputMaterial = document.getElementById("input-nome");
-    const inputQtd = document.getElementById("input-quantidade");
+    const inputQuantidade = document.getElementById("input-quantidade");
 
     const material = inputMaterial.value;
-    const quantidade = inputQtd.value;
+    const quantidade = inputQuantidade.value;
 
-    if(material === "" || quantidade ===""){
-        alert("Favor informar o material e a sua quantidade")
+    if (!quantidade || !material) {
+        alert("Preencha todos os campos");
         return;
     }
+//envia para o banco
+    await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ material: material, quantidade: quantidade })
+    });
 
-    addListaDeMaterias(material, quantidade);
+// 
     inputMaterial.value = "";
-    inputQtd.value = "";
-})
+    inputQuantidade.value = "";
 
+    
+    carregarTabela();
+});
+
+
+carregarTabela();
